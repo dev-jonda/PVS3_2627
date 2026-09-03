8a37a7e7ba094553ff9ca9600629f950c3012541
jonas.bouse <jonas.bouse@infis.cz>
2026-09-03 13:13:30 +0200

    Změna výpisu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Nothing to see here.");
+        System.out.println("Změna v kódu - zaludTest");
     }
 }
